9e2cfda7b69e5f669cd01068f7c57c6777d764a3
programador7 <programador7@contasiscorp.com>
2022-04-07 02:52:03 -0500

    revisar

--- lambda/index.js
+++ lambda/index.js
@@ -5,24 +5,29 @@
  * */
 const Alexa = require('ask-sdk-core');
 const axios = require('axios');
-const listaAyudaUrl= 'https://4e8f-186-148-196-74.ngrok.io/api/obtenerAyudaActividad/2/B/Comunicacion/Informacion/';
-const listaActividadUrl= 'https://4e8f-186-148-196-74.ngrok.io/api/obtenerDescripcionActividad/2/B/Comunicacion/Informacion/';
+//const listaActividadUrl = 'https://godartg.github.io/Ayuda_Alexa.github.io/Lista_actividad.json';
+//const listaAyudaUrl = 'https://godartg.github.io/Ayuda_Alexa.github.io/Lista_ayuda.json';
+//const listaAyudaUrl= 'https://4e8f-186-148-196-74.ngrok.io/api/obtenerAyudaActividad/2/B/Comunicacion/Informacion/1/2';
+//const listaActividadUrl= 'https://4e8f-186-148-196-74.ngrok.io/api/obtenerDescripcionActividad/2/B/Comunicacion/Informacion/1/2';
+const listaAyudaUrl= 'https://libro-comunicacion.herokuapp.com/api/obtenerAyudaActividad/';
+const listaActividadUrl= 'https://libro-comunicacion.herokuapp.com/api/obtenerDescripcionActividad/';
 /*=====================================================================*/
 
 
 
-const obtenerActividades = async (pagina, actividad) => {
+const obtenerActividades = async (pagina, actividad, curso, grado, seccion) => {
   try {
-    const { data } = await axios.get(listaActividadUrl+pagina+'/'+actividad);
+    const { data } = await axios.get(listaActividadUrl+grado+'/'+seccion+'/'+ curso +'/'+'Comunicaci%C3%B3n%203%20cuaderno%20de%20trabajo%20para%20tercer%20grado%20de%20educaci%C3%B3n%20primaria%202020'+ '/' + pagina +'/' + actividad);
+    //const data  = listaActividadUrl+ grado +'/'+ seccion +'/'+ curso +'/'+ 'Comunicaci%C3%B3n%203%20cuaderno%20de%20trabajo%20para%20tercer%20grado%20de%20educaci%C3%B3n%20primaria%202020' + '/' + pagina +'/' + actividad;
     return data;
   } catch (error) {
     console.error('no se pudo obtener datos', error);
   }
 };
 
-const obtenerAyudas = async (pagina, actividad) => {
+const obtenerAyudas = async (pagina, actividad, curso, grado, seccion) => {
   try {
-    const { data } = await axios.get(listaAyudaUrl+pagina+'/'+actividad);
+    const { data } = await axios.get(listaAyudaUrl+grado+'/'+seccion+'/'+ curso +'/'+ 'Comunicaci%C3%B3n%203%20cuaderno%20de%20trabajo%20para%20tercer%20grado%20de%20educaci%C3%B3n%20primaria%202020'+ '/' +pagina+'/'+actividad);
     return data;
   } catch (error) {
     console.error('no se pudo obtener datos', error);
@@ -34,7 +39,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Bienvenido, soy Alexa. Yo te ayudaré en tus actividades del libro de comunicación del tercer grado de primaria. Si tienes problemas en tus lecciones puedes pedirme ayuda. Prueba diciendo: Necesito ayuda' ;
+        const speakOutput = 'Bienvenido, soy Alexa. Yo te ayudaré en tus actividades. Prueba diciendo: Necesito ayuda' ;
         const speakOutput2 = 'Si tienes alguna duda con alguna actividad no dudes en preguntarme.';
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -71,11 +76,16 @@ const ConsultaManejador = {
     try {
         const DatoPagina = handlerInput.requestEnvelope.request.intent.slots.pagina.value;/*Número de página*/
         const DatoActividad= handlerInput.requestEnvelope.request.intent.slots.actividad.value;/*Actividad 1 , 2 , 3 ...*/
-        const DatoPregunta = handlerInput.requestEnvelope.request.intent.slots.pregunta.value;/*pregunta A B C ....*/
-        const lista_actividad = await obtenerActividades( DatoPagina, DatoActividad );
-        const lista_ayuda = await obtenerAyudas( DatoPagina, DatoActividad);
-        let speechText = `La actividad ${DatoActividad} de la página ${DatoPagina} dice: ${lista_actividad['detalle']}. ${lista_ayuda['ayuda']}`
-
+        const DatoCurso = handlerInput.requestEnvelope.request.intent.slots.curso.value;/*Curso Matemática, Comunicación, Ciencia y ambiente, personal social ....*/
+        const DatoSeccion = handlerInput.requestEnvelope.request.intent.slots.seccion.value;/*seccion A B C D E ....*/
+        const DatoGrado = handlerInput.requestEnvelope.request.intent.slots.grado.value;/*grado 1 2 3 4 5 6 ....*/
+        //let speechText = listaAyudaUrl +'/'+ DatoGrado +'/'+ DatoSeccion +'/'+ DatoCurso +'/'+ 'Comunicaci%C3%B3n%203%20cuaderno%20de%20trabajo%20para%20tercer%20grado%20de%20educaci%C3%B3n%20primaria%202020' + '/' +DatoPagina+'/'+DatoActividad;
+        //let speechText = listaActividadUrl +'/'+ DatoGrado +'/'+ DatoSeccion +'/'+ DatoCurso +'/'+ 'Comunicaci%C3%B3n%203%20cuaderno%20de%20trabajo%20para%20tercer%20grado%20de%20educaci%C3%B3n%20primaria%202020' + '/' +DatoPagina+'/'+DatoActividad;
+        const lista_actividad = await obtenerActividades( DatoPagina, DatoActividad, DatoCurso, DatoGrado, DatoSeccion );
+        const lista_ayuda = await obtenerAyudas( DatoPagina, DatoActividad, DatoCurso, DatoGrado, DatoSeccion);
+        let speechText = `La actividad ${DatoActividad} de la página ${DatoPagina} dice: ${lista_actividad['detalle']} ${lista_ayuda['ayuda']}`;
+        //let speechText = `${lista_actividad}`;
+        
         const responseBuilder = handlerInput.responseBuilder;
     
         //guardar dato en memoria
@@ -95,20 +105,7 @@ const ConsultaManejador = {
 };
 
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
 
 /*
 const ActividadUnoManejador = {
@@ -297,7 +294,6 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
         AyudaManejador,
         ConsultaManejador,
         /*HelpIntentHandler,*/
